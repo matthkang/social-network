ed63bbed04a2782af8ae89c8d59db04ded3dadf1
matthew <matthkang@gmail.com>
2023-10-23 22:36:12 -0700

    remove leaning for thought

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -15,16 +15,13 @@ module.exports = {
     async getSingleThought(req, res) {
         try {
             const thought = await Thought.findOne({ _id: req.params.thoughtId })
-                .select('-__v')
-                .lean();
+                .select('-__v');
 
             if (!thought) {
                 return res.status(404).json({ message: 'No thought with that ID' });
             }
 
-            res.json({
-                thought
-            });
+            res.json(thought);
         } catch (err) {
             console.log(err);
             return res.status(500).json(err);
